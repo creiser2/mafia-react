dcbdd2a9874d0ccdbe07a139ae8cef3b8768e1d6
github username <carlreiser4@gmail.com>
2018-10-17 16:52:38 -0400

    deploy branch

--- src/constants/api-endpoints.js
+++ src/constants/api-endpoints.js
@@ -1,16 +1,16 @@
-export const API_ROOT = 'http://localhost:3000/lobbies'
-export const USER_ROOT = 'http://localhost:3000/users'
-export const API_WS_ROOT = 'ws://localhost:3000/cable';
-export const API_AVAIL = 'http://localhost:3000/available'
-export const JOIN_LOBBY = 'http://localhost:3000/joinlobby'
-export const START_GAME = 'http://localhost:3000/startgame'
-export const GET_LOBBY_USERS = 'http://localhost:3000/lobbies/'
-export const UPDATE_USER_STATUS = 'http://localhost:3000/users/'
-export const PICK_MAFIA = 'http://localhost:3000/pickmafia'
-export const UPDATE_LOBBY_PROTECTION = 'http://localhost:3000/lobbies/'
-export const KILL_VICTIM = 'http://localhost:3000/killvictim'
-export const CAST_VOTE = 'http://localhost:3000/castvote'
-export const GET_MAFIA_KILL_STORY = 'http://localhost:3000/get-random-mafia-kill-story'
+export const API_ROOT = 'https://mafia-api.herokuapp.com/lobbies'
+export const USER_ROOT = 'https://mafia-api.herokuapp.com/users'
+export const API_WS_ROOT = 'wss://mafia-api.herokuapp.com/cable';
+export const API_AVAIL = 'https://mafia-api.herokuapp.com/available'
+export const JOIN_LOBBY = 'https://mafia-api.herokuapp.com/joinlobby'
+export const START_GAME = 'https://mafia-api.herokuapp.com/startgame'
+export const GET_LOBBY_USERS = 'https://mafia-api.herokuapp.com/lobbies/'
+export const UPDATE_USER_STATUS = 'https://mafia-api.herokuapp.com/users/'
+export const PICK_MAFIA = 'https://mafia-api.herokuapp.com/pickmafia'
+export const UPDATE_LOBBY_PROTECTION = 'https://mafia-api.herokuapp.com/lobbies/'
+export const KILL_VICTIM = 'https://mafia-api.herokuapp.com/killvictim'
+export const CAST_VOTE = 'https://mafia-api.herokuapp.com/castvote'
+export const GET_MAFIA_KILL_STORY = 'https://mafia-api.herokuapp.com/get-random-mafia-kill-story'
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
